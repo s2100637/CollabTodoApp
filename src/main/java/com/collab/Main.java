a236beddd43a72f499f52bf8072cfc3df064c0b8
SJarno <jarno.saastamoinen@hotmail.com>
2021-09-29 10:30:26 +0300

    test connection to db

--- src/main/java/com/collab/Main.java
+++ src/main/java/com/collab/Main.java
@@ -10,11 +10,13 @@ public class Main {
     
     public static void main(String[] args) throws SQLException {
 
+        /* Create connection: */
         Connection db = DriverManager.getConnection("jdbc:sqlite:testi.db");
 
+        /* Create tables and insert data */
         Statement statement = db.createStatement();
         statement.execute("CREATE TABLE Tuotteet (id INTEGER PRIMARY KEY, nimi TEXT, hinta INTEGER)");
-        statement.execute("INSERT INTO TUOTTEET (nimi, hinta) VALUES ('retiisi', 7)");
+        statement.execute("INSERT INTO TUOTTEET (nimi, hinta) VALUES ('Kaupasta kessua', 7)");
     }
     
 }
