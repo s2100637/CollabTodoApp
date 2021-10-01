0c770599e3025934e0ea8ddbcf743fc13f8b4da6
SJarno <jarno.saastamoinen@hotmail.com>
2021-10-01 13:00:34 +0300

    add test base for Connector

---
+++ src/test/java/com/collab/services/DatabaseConnectorTest.java
@@ -0,0 +1,23 @@
+package com.collab.services;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseConnectorTest {
+    private DatabaseConnector dummyConnector;
+    private String url = "";
+
+    @BeforeAll
+    void setUp() {
+
+    }
+    @Test
+    void testCreateConnection() {
+
+    }
+
+    @Test
+    void testInitDatabase() {
+
+    }
+}
